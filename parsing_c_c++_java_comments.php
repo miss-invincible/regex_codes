cf2a95e566766671708e8ca238d06a467950f5ab
miss-invincible <shivangi.gupta.jiit@gmail.com>
2017-01-05 04:10:37 +0530

    code to parse comments of C++ and java:made some changes to it

--- parsing_c_c++_java_comments.php
+++ parsing_c_c++_java_comments.php
@@ -2,24 +2,23 @@
 $_fp = fopen("php://stdin", "r");
 /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 #fscanf($_fp,"%d",$n);
-$line = fgets($_fp);
+#$line = fgets($_fp);
     
 $res=true;
 while(!feof($_fp)){
-
+$line = fgets($_fp);
 $pattern1 = "#^(//)(\S\s)*#";
 $pattern2 = "#(?<!(//))(//)[\S\s]*#";
 $pattern3 = "#(?<!(/\*))(/\*)[\S\s]*#";
 $pattern4 = "#[(\s\S)]*(?=(\*/))#";
 $result=array();
 if($res==false)
-    {
+{   
     print $line;
     if(preg_match($pattern4,$line,$result))
-            {
-            
-            $res=!$res;
-        }
+    {   
+        $res=!$res;
+   	}
 }
 else{
         if(preg_match($pattern1,$line, $result))
@@ -27,28 +26,26 @@ else{
             print $line;
         }
         else if(preg_match($pattern2,$line,$result))
-            {
-
-            print $result[0];
-
-            }
+        {
+        	print $result[0];
+        }
         else if(preg_match($pattern3,$line,$result))
-            {
+        {
             print $result[0];
             $res=!$res;
             if(preg_match($pattern4,$line,$result))
             {
-            
                 $res=!$res;
-             }
+            }
         }  
         else if(preg_match($pattern4,$line,$result))
-            {
-             print $result[0];
+        {
+            print $result[0];
             $res=!$res;
         }
     }
-    $line = fgets($_fp); 
+    
+    
 }
    
 
